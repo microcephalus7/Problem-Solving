d526b9e2a0e3dc0e5ebd63f419ed88b6d92317d4
honaldu <hsh9636@gmail.com>
2020-05-01 20:37:49 +0900

    for 뭄 (ing)

--- Development/PS/input2.js
+++ Development/PS/input2.js
@@ -1,18 +1,7 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().split(" ");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const n = input.filter((v) => v).map((v) => v.split(" ").map((v) => Number(v)));
 
-const hour = Number(input[0]); // 숫자를 몇 시간으로 바꿔야 함
-const min = Number(input[1]); // 숫자를 몇 분으로 바꿔야 함
-const plusHour = hour - 1;
-const plusMin = min - 45;
-const minusMin = 60 + min - 45;
-
-if (plusMin < 0) {
-  if (hour < 1) {
-    console.log(`23 ${minusMin}`);
-  } else {
-    console.log(`${plusHour} ${minusMin}`);
-  }
-} else {
-  console.log(`${hour} ${plusMin}`);
+for (let i = 1; i < n.length; i++) {
+  console.log(n[i][0] + n[i][1]);
 }
--- Development/PS/test.js
+++ Development/PS/test.js
@@ -1,15 +1,5 @@
-const hour = Number(prompt()); // 숫자를 몇 시간으로 바꿔야 함
-const min = Number(prompt()); // 숫자를 몇 분으로 바꿔야 함
-const plusHour = hour - 1;
-const plusMin = min - 45;
-const minusMin = 60 + min - 45;
+const number = Number(prompt());
 
-if (plusMin < 0) {
-  if (hour < 1) {
-    console.log(`23 ${minusMin}`);
-  } else {
-    console.log(`${plusHour} ${minusMin}`);
-  }
-} else {
-  console.log(`${hour} ${plusMin}`);
+for (let i = 1; i < 10; i++) {
+  console.log(`${number} X ${i} = ${number * i}`);
 }
