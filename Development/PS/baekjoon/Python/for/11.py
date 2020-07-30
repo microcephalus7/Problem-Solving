05669cf08d12c84bd3df9d93093555fc03e94aed
honaldu <hsh9636@gmail.com>
2020-07-30 14:40:21 +0900

    7월 30일

--- Development/PS/baekjoon/Python/for/11.py
+++ Development/PS/baekjoon/Python/for/11.py
@@ -1,5 +1,5 @@
 import sys
 
 a, b = map(int, sys.stdin.readline().split())
-score = [x for x in input().split() if int(x) < b]
-print(' '.join(score))
+score = [x for x in input().split() if int(x) < b] # List Comprehension
+print(' '.join(score)) # 구분자로 나눔.
---
+++ Development/PS/baekjoon/Python/while/2.py
@@ -0,0 +1,6 @@
+try:  # 예외문
+    while True:
+        a, b = map(int, input().split())
+        print(a+b)
+except:
+    exit()
