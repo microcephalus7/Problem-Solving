91cdca4e3bc1678f2623131d5ceebdba5ba8b84a
honaldu <hsh9636@gmail.com>
2020-10-07 23:19:57 +0900

    Readme

---
+++ Development/Web/JSInfo/2.4.js
@@ -0,0 +1,8 @@
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(name);
+
+let myPlanetName = "Earth";
+let currentUserName;
---
+++ Development/Web/JSInfo/2.6.js
@@ -0,0 +1,2 @@
+let name = prompt("이름 입력해라", "");
+alert(name);
